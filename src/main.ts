0c3edcd9963b48fef11e450c8dffb2c732f2bf8b
martinguenadev <martinguena.dev@gmail.com>
2023-07-24 20:14:57 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,22 @@ import { initializeApp } from 'firebase/app';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   initializeApp(firebaseConfig);
-
+  const whitelist = ['https://localhost:3000', 'https://www.localhost:3000'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    // credentials: true,
+  });
   await app.listen(8081);
 }
 bootstrap();
