29e597f4c794f9084df88152f7d771a87fbee70e
martinguenadev <martinguena.dev@gmail.com>
2023-07-26 20:49:05 -0300

    fix: new cors setup

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,11 @@ import { initializeApp } from 'firebase/app';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   initializeApp(firebaseConfig);
-  // const whitelist = ['http://localhost:3000', 'http://www.localhost:3000'];
-  app.enableCors();
-  // app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       console.log('allowed cors for:', origin);
-  //       callback(null, true);
-  //     } else {
-  //       console.log('blocked cors for:', origin);
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   allowedHeaders:
-  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Access-Control-Allow-Origin',
-  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-  //   // credentials: true,
-  // });
+  const whitelist = ['http://localhost:3000', 'http://www.localhost:3000'];
+  app.enableCors({
+    origin: whitelist,
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  });
   await app.listen(8081);
 }
 bootstrap();
